1160e33278282c6c587d857898ce31012221a515
fathima rehna <rehnaea76@gmail.com>
2025-10-14 09:47:39 +0530

    Added admin dashboard and updated static files

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+def login_view(request):
+
+
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(username='admin', password='12345678', email='admin@example.com')
+        print("Admin user created")
+
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            
+
+            
+            if username == 'admin' and password == '12345678':
+                messages.success(request, "Welcome Admin! Login successful.")
+                return redirect('admin_dashboard')
+            else:
+                return redirect('user_dashboard')
+        else:
+            messages.error(request, "Invalid username or password.")
+
+    return render(request, 'login.html')
+
+
+
+@login_required
+def admin_dashboard(request):
+    return render(request, 'admin_dashboard.html')
+
+@login_required
+def user_dashboard(request):
+    return render(request, 'user_dashboard.html')
+
+
+
--- myproject/settings.py
+++ myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'slbs_db',
+         'USER':'root',
+         'PASSWORD':'root',
+         'HOST':'localhost',
+         'PORT':'3306',
     }
 }
 
@@ -115,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[BASE_DIR/'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
