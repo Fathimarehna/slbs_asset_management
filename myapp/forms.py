67d83fbd9ca065c620523e58dd5eb83ebe3eb3d6
fathima rehna <rehnaea76@gmail.com>
2025-10-15 11:59:00 +0530

    changes

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+<<<<<<< HEAD
 from django.contrib.auth.models import User
 
 class UsercreationForm(forms.ModelForm):
@@ -8,3 +9,25 @@ class UsercreationForm(forms.ModelForm):
     
 
     
+=======
+from .models import User, Asset
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'password', 'role']
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput())
+
+class AssetForm(forms.ModelForm):
+    class Meta:
+        model = Asset
+        fields = [
+            'category', 'sub_category', 'asset_name', 'make_model',
+            'serial_number', 'location', 'assigned_to', 'department',
+            'purchase_date', 'warranty_expiry', 'condition', 'remarks'
+        ]
+>>>>>>> e8a5c04a1a4cfe8ea6cc7862e7250e109c836771
