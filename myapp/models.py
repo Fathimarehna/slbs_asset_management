064f4bfb86b9053388cec24cf53b0d2e68bb675e
athiraps2540 <athirasivan261@gmail.com>
2025-10-17 09:54:51 +0530

    Updated asset model and added migration

--- myapp/models.py
+++ myapp/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
        
 class Asset(models.Model):
-    title = models.CharField(max_length=100,default="Untitled Asset")
+    title = models.CharField(max_length=100) 
     status = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
-    def _str_(self):
-        return self.Title
+    def __str__(self):
+        return self.title
