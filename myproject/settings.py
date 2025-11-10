d0be4b1ad1b97cff9cdb2218c83b0b73b07f13f2
athiraps2540 <athirasivan261@gmail.com>
2025-11-10 09:28:24 +0530

    Initial commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'slbs_db',
          'USER':'root',
-         'PASSWORD':'root',
+         'PASSWORD':'root@123',
          'HOST':'localhost',
          'PORT':'3306',
     }
