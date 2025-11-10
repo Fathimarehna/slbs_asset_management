15590661f3ba5a68d4019a563feeb7b0460d18cb
fathima rehna <rehnaea76@gmail.com>
2025-11-10 09:50:37 +0530

    added user dashboard

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'slbs_db',
          'USER':'root',
-         'PASSWORD':'root@123',
+         'PASSWORD':'root',
          'HOST':'localhost',
          'PORT':'3306',
     }
